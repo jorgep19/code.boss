2599cca8a4a0486c875cca2cff7b9c1497c15357
Santiago <santiago7fergo@gmail.com>
2015-03-14 19:21:10 -0400

    Routes Ready, and Playing around with the Problems section

--- judgeClient/client/app/problems/problems.controller.js
+++ judgeClient/client/app/problems/problems.controller.js
@@ -2,5 +2,27 @@
 
 angular.module('codeBossApp')
   .controller('ProblemsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+
+      $scope.problem1 = false;	
+      $scope.problem2 = false;	
+      $scope.problem2 = false;		
+      $scope.problems = [
+        {id:1 ,problem:'Find the area',difficulty: 'Easy', description: 'Find the area of a cirlce'},
+        {id:2 ,problem:'2D arrays', difficulty: 'Medium' ,description: 'create a million 2D arrays'},
+        {id:3 ,problem:'The best problem', difficulty: 'Medium', description: 'Solve this amazing problem and get a million dollars'}
+    ];
+       
+       $scope.toggle = function(a) {
+       	if( a == 1 ){
+       		$scope.problem1  = !scope.problem1;
+       }
+       else {
+       	console.log("nada");
+       }
+       
+   };
+    
+
+  
+
   });
